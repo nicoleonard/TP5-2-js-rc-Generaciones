1ec53ee5d8d3678cbb23d3ba980e9a949b7cc767
nicoleonard <nikolasleonard@gmail.com>
2023-04-13 06:13:15 -0300

    añadidas algunas validaciones al formulario, creados los metodos esMayorDeEdad y mostrarDatos, añadida llamadas a las funciones correspondientes con el submit

--- js/Generaciones.js
+++ js/Generaciones.js
@@ -2,7 +2,7 @@ const generaciones = [{nombre:"Generacion Z",caracteristica:"Irreverencia",inter
                       {nombre:"Generacion Y",caracteristica:"Frustracion",intervalo:[1981,1993]},
                       {nombre:"Generacion X",caracteristica:"Obsesion por el exito",intervalo:[1969,1980]},
                       {nombre:"Baby Boom",caracteristica:"Ambicion",intervalo:[1949,1968]},
-                      {nombre:"Silent Generation",caracteristica:"Austeridad",intervalo:[1930/1948]}];
+                      {nombre:"Silent Generation",caracteristica:"Austeridad",intervalo:[1930,1948]}];
 
 class Persona{
     #nombre;
@@ -64,8 +64,8 @@ class Persona{
         let nacimiento = año - this.getEdad;
         let generacion = "";
         let caracteristica = "";
-        for(let i=0;i<generaciones.length;i++){
-            if(nacimiento>generaciones[i].intervalo[0] && nacimiento<generaciones[i].intervalo[1]){
+        for(let i=0;i<generaciones.length;i++){          
+            if(nacimiento>=generaciones[i].intervalo[0] && nacimiento<=generaciones[i].intervalo[1]){
                 generacion = generaciones[i].nombre;
                 caracteristica = generaciones[i].caracteristica;
             }
@@ -74,7 +74,11 @@ class Persona{
     }
 
     esMayorDeEdad(){
-
+        if(this.getEdad >= 21){
+            alert("Es mayor de edad");
+        }else{
+            alert("Es menor de edad");
+        }
     }
 
     mostrarDatos(){
@@ -87,16 +91,58 @@ let persona = null
 let ingreso = document.querySelector("#ingreso");
 ingreso.addEventListener("submit", (evento) =>{
     evento.preventDefault();
-
+    let valor_nombre = document.querySelector("#input_nombre");
+    let valor_edad = document.querySelector("#input_edad");
+    let valor_dni = document.querySelector("#input_dni");
+    let valor_sexo = document.querySelector("#input_sexo");
+    let valor_peso = document.querySelector("#input_peso");
+    let valor_altura = document.querySelector("#input_altura");
     switch(evento.submitter.id){
-        case "boton_crear":     persona=new Persona();
+        case "boton_crear":     if(persona != null){
+                                    confirm("Reemplazar persona creada?")
+                                }
+                                let verificar_datos = ()=>{
+
+                                    if(valor_nombre.value === "" || valor_nombre.value === null){
+                                        return false;
+                                    }
+                                    if(valor_edad.value === "" ||parseInt(valor_edad.value)<13 || parseInt(valor_edad.value)>93){
+                                        return false;
+                                    }
+                                    if(valor_dni.value === "" || parseInt(valor_dni.value)<0 || parseInt(valor_dni.value)>99999999 ){
+                                        return false;
+                                    }
+                                    if(valor_sexo.value.toUpperCase() != "M" && valor_sexo.value.toUpperCase() !="H"){
+                                        return false;
+                                    }
+                                    if(valor_peso.value === "" || parseInt(valor_peso.value)<30 || parseInt(valor_peso.value)>900){
+                                        return false;
+                                    }
+                                    if(valor_altura.value === "" ||parseFloat(valor_altura.value)<1 || parseFloat(valor_altura.value)>4){
+                                        return false;
+                                    }
+                                    return true;
+                                }
+
+                                if(!verificar_datos()){
+                                    alert("Ingrese los datos correctamente");
+                                    return;
+                                }
+                                persona=new Persona(valor_nombre.value,parseInt(valor_edad.value),valor_dni.value,valor_sexo.value.toUpperCase(),valor_peso.value,valor_altura.value);
+                                alert("Persona creada con exito");
                                 break;
+
         case "boton_mostrar":   if(persona === null){
                                     alert("Debe ingresar los datos de una persona antes de consultar su generacion")
+                                }else{
+                                    persona.mostrarGeneracion();
                                 };
                                 break;
+
         case "boton_verificar": if(persona === null){
                                     alert("Debe ingresar los datos de una persona antes de verificar la mayoria de edad")
+                                }else{
+                                    persona.esMayorDeEdad();
                                 };
                                 break;
     }
